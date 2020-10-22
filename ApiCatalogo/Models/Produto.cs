7c8699a80b46b0a057cccf7af0040e459143c321
Tiago Vaz <tiago.vaz@hotmail.com>
2020-10-22 15:45:41 -0300

    Aplicando validações (DataAnnotatios, Propriedades customizadas e implementaçao da interface IValidatableObject)

--- ApiCatalogo/Models/Produto.cs
+++ ApiCatalogo/Models/Produto.cs
@@ -1,27 +1,30 @@
-﻿using System;
+﻿using ApiCatalogo.Validations;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ApiCatalogo.Models
 {
     [Table("Produtos")]
-    public class Produto
+    public class Produto : IValidatableObject
     {
         [Key]
         public int ProdutoId { get; set; }
 
-        [Required]
-        [MaxLength(80)]
+        //[MaxLength(80)] -> Define o tamanho no banco enquanto o StringLength na validação
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [StringLength(20, ErrorMessage = "O nome deve ter entre 5 e 20 caracteres", MinimumLength = 5)]
+        //[PrimeiraLetraMaiuscula]
         public string Nome { get; set; }
 
         [Required]
-        [MaxLength(300)]
+        //[MaxLength(300)]
+        [StringLength(20, ErrorMessage = "A descrição deve ter no máximo {1}")]
         public string Descricao { get; set; }
 
         [Required]
+        [Range(1, 10000, ErrorMessage = "O preço deve estar entre {1} e {2}")]
         public double Preco { get; set; }
 
         [Required]
@@ -35,5 +38,29 @@ namespace ApiCatalogo.Models
         // Definindo a FK
         public Categoria Categoria { get; set; }
         public int CategoriaId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(this.Nome))
+            {
+                var primeiraLetra = this.Nome[0].ToString();
+                if (primeiraLetra != primeiraLetra.ToUpper())
+                {
+                    yield return new
+                        ValidationResult("A primeira letra do nome do produto deve ser maiúscula",
+                        new[] { nameof(this.Nome) }
+                        );
+                }
+            }
+
+            if(this.Estoque <= 0)
+            {
+                yield return new
+                    ValidationResult("O Estoque deve ser maior que zero.",
+                    new[] { nameof(this.Estoque) }
+                    );
+            }
+
+        }
     }
 }
