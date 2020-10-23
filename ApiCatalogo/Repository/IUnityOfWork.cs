f4d4d02ef7cd40eab78ce488351ab6a60b4fe11e
Tiago Vaz <tiago.vaz@hotmail.com>
2020-10-23 11:51:45 -0300

    Padrão Unit Of Work implementado

---
+++ ApiCatalogo/Repository/IUnityOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiCatalogo.Repository
+{
+    public interface IUnityOfWork
+    {
+        // Uma instancia de cada repositório criado, caso um novo seja criado, é necessário adicionar aqui!
+        IProdutoRepository ProdutoRepository { get; }
+        ICategoriaRepository CategoriaRepository { get; }
+
+        void Commit();
+    }
+}
---
+++ ApiCatalogo/Repository/UnityOfWork.cs
@@ -0,0 +1,41 @@
+﻿using ApiCatalogo.Context;
+
+namespace ApiCatalogo.Repository
+{
+    public class UnityOfWork : IUnityOfWork
+    {
+        private ProdutoRepository _produtoRepository;
+        private CategoriaReposository _categoryRepository;
+        public AppDbContext _context;
+
+        public UnityOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IProdutoRepository ProdutoRepository { 
+            get 
+            {
+                return _produtoRepository = _produtoRepository ?? new ProdutoRepository(_context);
+            } 
+        }
+
+        public ICategoriaRepository CategoriaRepository
+        {
+            get
+            {
+                return _categoryRepository ??= new CategoriaReposository(_context);
+            }
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
