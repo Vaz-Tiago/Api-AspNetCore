4d5625b9ed987f69ce9ecbf9c8e31d0f3f608ecf
Tiago Vaz <tiago.vaz@hotmail.com>
2020-10-23 11:07:05 -0300

    Implementado padrão repositório, syncrono. Interface e Implementação genérica

---
+++ ApiCatalogo/Repository/Repository.cs
@@ -0,0 +1,45 @@
+﻿using ApiCatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ApiCatalogo.Repository
+{
+    // Define que o repositório só pode ser do tipo classe
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected AppDbContext _context;
+
+        public Repository(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        public IQueryable<T> Get()
+        {
+            return _context.Set<T>().AsNoTracking();
+        }
+
+        public T GetById(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().SingleOrDefault(predicate);
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.Set<T>().Update(entity);
+        }
+    }
+}
