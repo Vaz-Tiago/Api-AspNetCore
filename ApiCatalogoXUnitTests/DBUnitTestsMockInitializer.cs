6694d8e7ac7448d2349231bbae693de429fd9d4d
Tiago Vaz <tiago.vaz@hotmail.com>
2020-11-05 18:08:03 -0300

    Adicionado projeto de testes

---
+++ ApiCatalogoXUnitTests/DBUnitTestsMockInitializer.cs
@@ -0,0 +1,13 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiCatalogoXUnitTests
+{
+    public class DBUnitTestsMockInitializer
+    {
+
+    }
+}
